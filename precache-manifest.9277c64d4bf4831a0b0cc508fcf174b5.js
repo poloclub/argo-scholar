25f520e2402444bcfd778616a2afc45c606b0489
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2022-11-28 17:09:01 +0000

    Deploy poloclub/argo-scholar to github.com/poloclub/argo-scholar.git:gh-pages

---
+++ precache-manifest.9277c64d4bf4831a0b0cc508fcf174b5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "107c7f1f6b8e272b13ff6780dc72685c",
+    "url": "/argo-scholar/index.html"
+  },
+  {
+    "revision": "b0f5c361beb63eb76658",
+    "url": "/argo-scholar/static/css/2.03e46918.chunk.css"
+  },
+  {
+    "revision": "15e50cf3ea81d3c7b1ac",
+    "url": "/argo-scholar/static/css/main.8a5be818.chunk.css"
+  },
+  {
+    "revision": "b0f5c361beb63eb76658",
+    "url": "/argo-scholar/static/js/2.141b314d.chunk.js"
+  },
+  {
+    "revision": "15e50cf3ea81d3c7b1ac",
+    "url": "/argo-scholar/static/js/main.d8d4862f.chunk.js"
+  },
+  {
+    "revision": "c6ef5b800d3dd0f2d251",
+    "url": "/argo-scholar/static/js/runtime~main.19d20500.js"
+  },
+  {
+    "revision": "4d2f6cdc35f7d06d85ff6d5b49a2e443",
+    "url": "/argo-scholar/static/media/argoscholar_logo_dark.4d2f6cdc.svg"
+  },
+  {
+    "revision": "a7447f198b3f130e367b87f50a558263",
+    "url": "/argo-scholar/static/media/argoscholar_logo_light.a7447f19.svg"
+  },
+  {
+    "revision": "9f52e4014adab067c8ef0f48ec1a4f43",
+    "url": "/argo-scholar/static/media/icons-16.9f52e401.eot"
+  },
+  {
+    "revision": "b49a90ee8c246157637b5ed142005d12",
+    "url": "/argo-scholar/static/media/icons-16.b49a90ee.woff"
+  },
+  {
+    "revision": "d75e6370295e046dd0b170ee42c3fa44",
+    "url": "/argo-scholar/static/media/icons-16.d75e6370.ttf"
+  },
+  {
+    "revision": "0f7149769d6b4ee77b646ee5b90c005a",
+    "url": "/argo-scholar/static/media/icons-20.0f714976.ttf"
+  },
+  {
+    "revision": "9baec0ab0bc3096fb9fe865d6ae29d1d",
+    "url": "/argo-scholar/static/media/icons-20.9baec0ab.woff"
+  },
+  {
+    "revision": "c6e7874eefb0dacfd50dad73a2de5a17",
+    "url": "/argo-scholar/static/media/icons-20.c6e7874e.eot"
+  }
+]);
